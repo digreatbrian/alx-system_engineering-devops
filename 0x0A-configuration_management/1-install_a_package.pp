a972783150de5678f701556ae4df01f0d125a8fb
digreatbrian <digreatbrian@gmail.com>
2024-03-23 01:27:22 -0700

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # Using Puppet, install flask from pip3
 exec { 'install python packages':
-     command   => 'pip3 install Werkzeug && pip3 install flask == 2.1.0',
+     command   => 'pip3 install Werkzeug==2.1.1 && pip3 install flask == 2.1.0',
      path => ['/usr/bin/'],
 }
