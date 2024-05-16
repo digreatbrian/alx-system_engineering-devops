c7289f0a273e16c0c78de26ef5d1c1fb8f29b613
Brian Musakwa <digreatbrian@gmail.com>
2024-05-16 19:03:37 +0000

    Some few changes

--- 0x1A-application_server/replace_nginx_config.pp
+++
@@ -1,9 +0,0 @@
-file { '/etc/nginx/nginx.conf':
-  ensure  => present,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => template('nginx-replace/nginx.conf.erb'),
-  notify  => Service['nginx'],
-}
-
